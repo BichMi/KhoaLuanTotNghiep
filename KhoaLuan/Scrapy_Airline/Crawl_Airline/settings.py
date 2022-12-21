42122df47ad8069a17edada250afd024edeb6bcc
mi.nguyen <mi.nguyen@ilavietnam.edu.vn>
2022-12-21 11:13:37 +0700

    test 4

--- KhoaLuan/Scrapy_Airline/Crawl_Airline/settings.py
+++ KhoaLuan/Scrapy_Airline/Crawl_Airline/settings.py
@@ -5,6 +5,7 @@
 
 # Test branch 
 # Test branch3 
+# Test branch 4
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
 #
